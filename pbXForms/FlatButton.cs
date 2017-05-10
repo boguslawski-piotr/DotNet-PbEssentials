9ec0671be3095fed6ba9bd0ce28b4c3ca0a3c013
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-05-10 12:20:38 -0400

    pbXForms library initial version & packages update

---
+++ pbXForms/FlatButton.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace pbXForms
+{
+#if __IOS__
+	public class FlatButton : Button
+#else
+	public class FlatButton : Label
+#endif
+	{
+		public FlatButton()
+		{
+			HeightRequest = Metrics.TouchTargetHeight;
+			MinimumWidthRequest = Metrics.TouchTargetHeight;
+			VerticalOptions = LayoutOptions.Center;
+#if !__IOS__
+			VerticalTextAlignment = TextAlignment.Center;
+#endif
+		}
+	}
+}
---
+++ pbXForms/Metrics.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace pbXForms
+{
+	public class Metrics
+	{
+#if __IOS__
+		public static int StatusBarHeight = 20;
+#endif
+#if __ANDROID__
+		public static int StatusBarHeight = 24;
+#endif
+		public static int ScreenEdgeLeftRightMargin = 16;
+
+		public static int AppBarHeightPortrait = 56;
+		public static int AppBarHeightLandscape = 48;
+
+		public static int TouchTargetHeight = 48;
+	}
+}
