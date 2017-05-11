85a94751d91d05243126ecf17a435b23883d2888
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-05-11 16:32:27 -0400

    UI prototype iteration 5

--- pbXForms/FlatButton.cs
+++ pbXForms/FlatButton.cs
@@ -6,20 +6,113 @@ namespace pbXForms
 #if __IOS__
 	public class FlatButton : Button
 #else
-	public class FlatButton : Label
+	public class FlatButton : ContentView
 #endif
 	{
 #if !__IOS__
+		private Label _Label
+		{
+			get
+			{
+				return (Xamarin.Forms.Label)(Content as StackLayout).Children[1];
+			}
+		}
+	
+		public string Text
+		{
+			set
+			{
+				_Label.Text = value;
+				_Label.IsVisible = true;
+			}
+		}
+
+		public Color TextColor
+		{
+			set
+			{
+				_Label.TextColor = value;
+			}
+		}
+
+		public FontAttributes FontAttributes 
+		{ 
+			set
+			{
+				_Label.FontAttributes = value;
+			}
+		}
+
+		public string FontFamily
+		{
+			set
+			{
+				_Label.FontFamily = value;
+			}
+		}
+
+		public double FontSize
+		{
+			set
+			{
+				_Label.FontSize = value;
+			}
+		}
+
+		private Image _Image
+		{
+			get 
+			{
+				return (Xamarin.Forms.Image)(Content as StackLayout).Children[0];
+			}
+		}
+
+		public FileImageSource Image
+		{
+			set {
+				_Image.Source = value;
+				_Image.IsVisible = true;
+			}
+		}
+
 		public event EventHandler Clicked;
 #endif
+
 		public FlatButton()
 		{
 			HeightRequest = Metrics.TouchTargetHeight;
 			MinimumWidthRequest = Metrics.TouchTargetHeight;
-			VerticalOptions = LayoutOptions.Center;
-#if !__IOS__
-			VerticalTextAlignment = TextAlignment.Center;
 
+			VerticalOptions = LayoutOptions.Center;
+
+			Margin = new Thickness(0);
+
+#if !__IOS__
+
+			Content = new StackLayout()
+			{
+				Orientation = StackOrientation.Horizontal,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
+
+				Margin = new Thickness(0),
+				Padding = new Thickness(0),
+				Spacing = Metrics.ButtonItemsSpacing,
+
+				Children = {
+					new Image() {
+						IsVisible = false,
+						HeightRequest = Metrics.IconHeight,
+						WidthRequest = Metrics.IconHeight,
+						Aspect = Aspect.AspectFit,
+					},
+					new Label() {
+						IsVisible = false,
+						VerticalTextAlignment = TextAlignment.Center,
+					}
+				}
+			};
+				
 			TapGestureRecognizer tgr = new TapGestureRecognizer()
 			{
 				Command = new Command(OnTapped)
--- pbXForms/Metrics.cs
+++ pbXForms/Metrics.cs
@@ -9,14 +9,18 @@ namespace pbXForms
 #if __ANDROID__
 		public static int StatusBarHeight = 24;
 #endif
-		public static int ScreenEdgeLeftRightMargin = 16;
+		public static int ScreenEdgeMargin = 16;
 
 		public static int AppBarHeightPortrait = 56;
 		public static int AppBarHeightLandscape = 48;
 
 		public static int ToolBarHeightPortrait = 48;
 		public static int ToolBarHeightLandscape = 48;
+		public static int ToolBarItemsSpacing = 24;
 
 		public static int TouchTargetHeight = 48;
+		public static int IconHeight = 24;
+
+		public static int ButtonItemsSpacing = 8;
 	}
 }
