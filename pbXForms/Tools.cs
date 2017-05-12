a7b24a6cdbfa14775a4db33579c9284633635334
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-05-12 01:50:23 -0400

    New utility class

--- pbXForms/Tools.cs
+++ pbXForms/Tools.cs
@@ -1,10 +1,48 @@
 ﻿using System;
+
+#if __ANDROID__
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+#endif
+
+#if __IOS__
+using UIKit;
+#endif
+
 namespace pbXForms
 {
-	public class Tools
+	public enum DeviceOrientations
 	{
-		public Tools()
+		Undefined,
+		Landscape,
+		Portrait
+	}
+
+	static public class Tools
+	{
+		static public DeviceOrientations DeviceOrientation
 		{
+			get
+			{
+#if __IOS__
+				var currentOrientation = UIApplication.SharedApplication.StatusBarOrientation;
+
+				bool isPortrait =
+					currentOrientation == UIInterfaceOrientation.Portrait
+					|| currentOrientation == UIInterfaceOrientation.PortraitUpsideDown;
+
+				return isPortrait ? DeviceOrientations.Portrait : DeviceOrientations.Landscape;
+#endif
+#if __ANDROID__
+				IWindowManager windowManager = Android.App.Application.Context.GetSystemService(Context.WindowService).JavaCast<IWindowManager>();
+
+				var rotation = windowManager.DefaultDisplay.Rotation;
+				bool isLandscape = rotation == SurfaceOrientation.Rotation90 || rotation == SurfaceOrientation.Rotation270;
+			            
+				return isLandscape ? DeviceOrientations.Landscape : DeviceOrientations.Portrait;
+#endif
+			}
 		}
 	}
 }
