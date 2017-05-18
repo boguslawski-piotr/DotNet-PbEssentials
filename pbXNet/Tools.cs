70170ea390897ff963af27f664ef4c963fb5bdfe
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-05-18 01:29:31 -0400

    pbXNet: Observable / Singleton tools

--- pbXNet/Tools.cs
+++ pbXNet/Tools.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿﻿using System;
+using System.Collections.Concurrent;
+using System.ComponentModel;
 using System.Globalization;
 using System.IO;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace pbXNet
@@ -14,7 +17,37 @@ namespace pbXNet
             b = a;
             return true;
         }
-
-
     }
+
+	public class Observable : INotifyPropertyChanged
+	{
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		protected void Set<T>(ref T storage, T value, [CallerMemberName]string propertyName = null)
+		{
+			if (Equals(storage, value))
+			{
+				return;
+			}
+
+			storage = value;
+			OnPropertyChanged(propertyName);
+		}
+
+		virtual protected void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+	}
+
+	internal static class Singleton<T> where T : new()
+	{
+		private static ConcurrentDictionary<Type, T> _instances = new ConcurrentDictionary<Type, T>();
+
+		public static T Instance
+		{
+			get
+			{
+				return _instances.GetOrAdd(typeof(T), (t) => new T());
+			}
+		}
+	}
+
 }
