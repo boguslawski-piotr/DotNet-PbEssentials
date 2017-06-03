4b0e039412c19ad8c953c66fe74deb24731ce877
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-02 20:17:09 -0400

    Projects options, formatting rules

--- pbXForms/PIButton.cs
+++ pbXForms/PIButton.cs
@@ -8,184 +8,184 @@ using static Xamarin.Forms.Button;
 
 namespace pbXForms
 {
-    public class PIButton : ContentView
-    {
-        public static readonly BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(ICommand), typeof(PIButton), null,
-            propertyChanged: (bo, o, n) => ((PIButton)bo).OnCommandChanged());
+	public class PIButton : ContentView
+	{
+		public static readonly BindableProperty CommandProperty = BindableProperty.Create("Command", typeof(ICommand), typeof(PIButton), null,
+			propertyChanged: (bo, o, n) => ((PIButton)bo).OnCommandChanged());
 
-        public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create("CommandParameter", typeof(object), typeof(PIButton), null,
-            propertyChanged: (bo, o, n) => ((PIButton)bo).CommandCanExecuteChanged(bo, EventArgs.Empty));
+		public static readonly BindableProperty CommandParameterProperty = BindableProperty.Create("CommandParameter", typeof(object), typeof(PIButton), null,
+			propertyChanged: (bo, o, n) => ((PIButton)bo).CommandCanExecuteChanged(bo, EventArgs.Empty));
 
-        public ICommand Command
-        {
-            get { return (ICommand)GetValue(CommandProperty); }
-            set { SetValue(CommandProperty, value); }
-        }
+		public ICommand Command
+		{
+			get { return (ICommand)GetValue(CommandProperty); }
+			set { SetValue(CommandProperty, value); }
+		}
 
-        public object CommandParameter
-        {
-            get { return GetValue(CommandParameterProperty); }
-            set { SetValue(CommandParameterProperty, value); }
-        }
+		public object CommandParameter
+		{
+			get { return GetValue(CommandParameterProperty); }
+			set { SetValue(CommandParameterProperty, value); }
+		}
 
-        public event EventHandler Clicked;
+		public event EventHandler Clicked;
 
-        public static readonly BindableProperty ContentLayoutProperty = BindableProperty.Create("ContentLayout", typeof(ButtonContentLayout), typeof(PIButton),
-            new ButtonContentLayout(ButtonContentLayout.ImagePosition.Left, Metrics.ButtonItemsSpacing), 
-            propertyChanged: (bo, o, n) => ((PIButton)bo).OnContentLayoutChanged());
+		public static readonly BindableProperty ContentLayoutProperty = BindableProperty.Create("ContentLayout", typeof(ButtonContentLayout), typeof(PIButton),
+			new ButtonContentLayout(ButtonContentLayout.ImagePosition.Left, Metrics.ButtonItemsSpacing),
+			propertyChanged: (bo, o, n) => ((PIButton)bo).OnContentLayoutChanged());
 
-        public ButtonContentLayout ContentLayout
-        {
-            get { return (ButtonContentLayout)GetValue(ContentLayoutProperty); }
-            set { SetValue(ContentLayoutProperty, value); }
-        }
+		public ButtonContentLayout ContentLayout
+		{
+			get { return (ButtonContentLayout)GetValue(ContentLayoutProperty); }
+			set { SetValue(ContentLayoutProperty, value); }
+		}
 
-        // TODO: bindable?
+		// TODO: bindable?
 
-        private Label _Label { get; set; }
+		Label _Label { get; set; }
 
-        public string Text
-        {
-            get { return _Label.Text; }
-            set {
-                _Label.Text = value;
-                _Label.IsVisible = _Label.Text?.Length > 0;
-            }
-        }
+		public string Text
+		{
+			get { return _Label.Text; }
+			set {
+				_Label.Text = value;
+				_Label.IsVisible = _Label.Text?.Length > 0;
+			}
+		}
 
-        public Color TextColor
-        {
-            get { return _Label.TextColor; }
-            set { _Label.TextColor = value; }
-        }
+		public Color TextColor
+		{
+			get { return _Label.TextColor; }
+			set { _Label.TextColor = value; }
+		}
 
-        public LineBreakMode LineBreakMode
-        {
-            get { return _Label.LineBreakMode; }
-            set { _Label.LineBreakMode = value; }
-        }
+		public LineBreakMode LineBreakMode
+		{
+			get { return _Label.LineBreakMode; }
+			set { _Label.LineBreakMode = value; }
+		}
 
-        public FontAttributes FontAttributes
-        {
-            get { return _Label.FontAttributes; }
-            set { _Label.FontAttributes = value; }
-        }
+		public FontAttributes FontAttributes
+		{
+			get { return _Label.FontAttributes; }
+			set { _Label.FontAttributes = value; }
+		}
 
-        public string FontFamily
-        {
-            get { return _Label.FontFamily; }
-            set { _Label.FontFamily = value; }
-        }
+		public string FontFamily
+		{
+			get { return _Label.FontFamily; }
+			set { _Label.FontFamily = value; }
+		}
 
-        [TypeConverter(typeof(FontSizeConverter))]
-        public double FontSize
-        {
-            get { return _Label.FontSize; }
-            set { _Label.FontSize = value; }
-        }
+		[TypeConverter(typeof(FontSizeConverter))]
+		public double FontSize
+		{
+			get { return _Label.FontSize; }
+			set { _Label.FontSize = value; }
+		}
 
-        private Image _Image { get; set; }
+		Image _Image { get; set; }
 
-        private FileImageSource _ImageSource = null;
-        public FileImageSource Image
-        {
-            get { return _ImageSource; }
-            set {
-                _ImageSource = value;
-                _Image.Source = value;
-                _Image.IsVisible = true;
-            }
-        }
+		FileImageSource _ImageSource;
+		public FileImageSource Image
+		{
+			get { return _ImageSource; }
+			set {
+				_ImageSource = value;
+				_Image.Source = value;
+				_Image.IsVisible = true;
+			}
+		}
 
-        public PIButton()
-        {
-            HeightRequest = Metrics.TouchTargetHeight;
-            MinimumHeightRequest = Metrics.TouchTargetHeight;
-            WidthRequest = Metrics.TouchTargetHeight;
-            MinimumWidthRequest = Metrics.TouchTargetHeight;
+		public PIButton()
+		{
+			HeightRequest = Metrics.TouchTargetHeight;
+			MinimumHeightRequest = Metrics.TouchTargetHeight;
+			WidthRequest = Metrics.TouchTargetHeight;
+			MinimumWidthRequest = Metrics.TouchTargetHeight;
 
-            VerticalOptions = LayoutOptions.Center;
-            Margin = new Thickness(0);
+			VerticalOptions = LayoutOptions.Center;
+			Margin = new Thickness(0);
 
-            _Image = new Image()
-            {
-                IsVisible = false,
-                HeightRequest = Metrics.IconHeight,
-                WidthRequest = Metrics.IconHeight,
-                Aspect = Aspect.AspectFit,
-            };
+			_Image = new Image()
+			{
+				IsVisible = false,
+				HeightRequest = Metrics.IconHeight,
+				WidthRequest = Metrics.IconHeight,
+				Aspect = Aspect.AspectFit,
+			};
 
-            _Label = new Label()
-            {
-                IsVisible = false,
-                VerticalTextAlignment = TextAlignment.Center,
-                HorizontalTextAlignment = TextAlignment.Center,
-            };
+			_Label = new Label()
+			{
+				IsVisible = false,
+				VerticalTextAlignment = TextAlignment.Center,
+				HorizontalTextAlignment = TextAlignment.Center,
+			};
 
-            Content = new StackLayout()
-            {
-                Orientation = StackOrientation.Horizontal,
-                VerticalOptions = LayoutOptions.CenterAndExpand,
-                HorizontalOptions = LayoutOptions.CenterAndExpand,
+			Content = new StackLayout()
+			{
+				Orientation = StackOrientation.Horizontal,
+				VerticalOptions = LayoutOptions.CenterAndExpand,
+				HorizontalOptions = LayoutOptions.CenterAndExpand,
 
-                Margin = new Thickness(0),
-                Padding = new Thickness(0),
-                Spacing = ContentLayout.Spacing,
+				Margin = new Thickness(0),
+				Padding = new Thickness(0),
+				Spacing = ContentLayout.Spacing,
 
-                Children = {
-                    _Image,
-                    _Label
-                }
-            };
+				Children = {
+					_Image,
+					_Label
+				}
+			};
 
-            TapGestureRecognizer tgr = new TapGestureRecognizer();
-            tgr.Command = new Command(OnTapped);
-            this.GestureRecognizers.Add(tgr);
-        }
+			TapGestureRecognizer tgr = new TapGestureRecognizer();
+			tgr.Command = new Command(OnTapped);
+			this.GestureRecognizers.Add(tgr);
+		}
 
-        protected override void OnPropertyChanging(string propertyName = null)
-        {
-            if (propertyName == CommandProperty.PropertyName)
-            {
-                ICommand cmd = Command;
-                if (cmd != null)
-                    cmd.CanExecuteChanged -= CommandCanExecuteChanged;
-            }
+		protected override void OnPropertyChanging(string propertyName = null)
+		{
+			if (propertyName == CommandProperty.PropertyName)
+			{
+				ICommand cmd = Command;
+				if (cmd != null)
+					cmd.CanExecuteChanged -= CommandCanExecuteChanged;
+			}
 
-            base.OnPropertyChanging(propertyName);
-        }
+			base.OnPropertyChanging(propertyName);
+		}
 
-        bool _IsEnabled
-        {
-            set {
-                IsEnabled = value;
-                Content.IsEnabled = IsEnabled;
-                _Image.IsEnabled = IsEnabled;
-                _Label.IsEnabled = IsEnabled;
-            }
-        }
+		bool _IsEnabled
+		{
+			set {
+				IsEnabled = value;
+				Content.IsEnabled = IsEnabled;
+				_Image.IsEnabled = IsEnabled;
+				_Label.IsEnabled = IsEnabled;
+			}
+		}
 
-        void CommandCanExecuteChanged(object sender, EventArgs eventArgs)
-        {
-            ICommand cmd = Command;
-            if (cmd != null)
-            {
-                _IsEnabled = cmd.CanExecute(CommandParameter);
-            }
-        }
+		void CommandCanExecuteChanged(object sender, EventArgs eventArgs)
+		{
+			ICommand cmd = Command;
+			if (cmd != null)
+			{
+				_IsEnabled = cmd.CanExecute(CommandParameter);
+			}
+		}
 
-        void OnCommandChanged()
-        {
-            if (Command != null)
-            {
-                Command.CanExecuteChanged += CommandCanExecuteChanged;
-                CommandCanExecuteChanged(this, EventArgs.Empty);
-            }
-            else
-                _IsEnabled = true;
-        }
+		void OnCommandChanged()
+		{
+			if (Command != null)
+			{
+				Command.CanExecuteChanged += CommandCanExecuteChanged;
+				CommandCanExecuteChanged(this, EventArgs.Empty);
+			}
+			else
+				_IsEnabled = true;
+		}
 
-        void OnContentLayoutChanged()
+		void OnContentLayoutChanged()
 		{
 			if (Content is StackLayout c)
 			{
@@ -204,21 +204,21 @@ namespace pbXForms
 
 		volatile Int32 _onTappedIsRunning = 0;
 
-        void OnTapped(object parameter)
-        {
-            if (!IsEnabled || Interlocked.Exchange(ref _onTappedIsRunning, 1) == 1)
-                return;
+		void OnTapped(object parameter)
+		{
+			if (!IsEnabled || Interlocked.Exchange(ref _onTappedIsRunning, 1) == 1)
+				return;
 
-            StartTappedAnimation();
+			StartTappedAnimation();
 
-            Command?.Execute(CommandParameter);
-            Clicked?.Invoke(this, EventArgs.Empty);
+			Command?.Execute(CommandParameter);
+			Clicked?.Invoke(this, EventArgs.Empty);
 
-            Interlocked.Exchange(ref _onTappedIsRunning, 0);
-        }
+			Interlocked.Exchange(ref _onTappedIsRunning, 0);
+		}
 
-        protected virtual void StartTappedAnimation()
-        {
+		protected virtual void StartTappedAnimation()
+		{
 			Task.Run(async () =>
 			{
 				await this.ScaleTo(1.25, (uint)(DeviceEx.AnimationsLength * 0.65), Easing.CubicOut);
@@ -226,8 +226,8 @@ namespace pbXForms
 			});
 
 		}
-    }
+	}
 
-    [Obsolete("FlatButton is obsolete. Please use PIButton (as Platform Independent Button) instead.")]
+	[Obsolete("FlatButton is obsolete. Please use PIButton (as Platform Independent Button) instead.")]
 	public class FlatButton : PIButton { }
 }
--- pbXNet/LocalizationManager.cs
+++ pbXNet/LocalizationManager.cs
@@ -7,109 +7,109 @@ using System.Resources;
 
 namespace pbXNet
 {
-    public static class LocalizationManager
-    {
-        static Locale _locale = new Locale();
+	public static class LocalizationManager
+	{
+		static Locale _locale = new Locale();
 
-        public static CultureInfo CurrentCultureInfo
-        {
-            get {
-                CultureInfo c = _locale.GetCurrentCultureInfo();
-                _locale.SetLocale(c);
-                return c;
-            }
-        }
+		public static CultureInfo CurrentCultureInfo
+		{
+			get {
+				CultureInfo c = _locale.GetCurrentCultureInfo();
+				_locale.SetLocale(c);
+				return c;
+			}
+		}
 
-        static CultureInfo _cultureInfo;
+		static CultureInfo _cultureInfo;
 
-        public static CultureInfo CultureInfo
-        {
-            get {
-                if (_cultureInfo == null)
-                    _cultureInfo = CurrentCultureInfo;
-                return _cultureInfo;
-            }
-            set {
-                _cultureInfo = value;
-            }
-        }
+		public static CultureInfo CultureInfo
+		{
+			get {
+				if (_cultureInfo == null)
+					_cultureInfo = CurrentCultureInfo;
+				return _cultureInfo;
+			}
+			set {
+				_cultureInfo = value;
+			}
+		}
 
-        class Resource
-        {
-            public string BaseName { get; set; }
-            public Assembly Assembly { get; set; }
+		class Resource
+		{
+			public string BaseName { get; set; }
+			public Assembly Assembly { get; set; }
 
-            ResourceManager _ResourceManager;
-            public ResourceManager ResourceManager
-            {
-                get {
-                    if (object.ReferenceEquals(_ResourceManager, null))
-                    {
-                        ResourceManager rm = new ResourceManager(BaseName, Assembly);
-                        _ResourceManager = rm;
-                    }
-                    return _ResourceManager;
-                }
+			ResourceManager _ResourceManager;
+			public ResourceManager ResourceManager
+			{
+				get {
+					if (object.ReferenceEquals(_ResourceManager, null))
+					{
+						ResourceManager rm = new ResourceManager(BaseName, Assembly);
+						_ResourceManager = rm;
+					}
+					return _ResourceManager;
+				}
 
-            }
-        }
+			}
+		}
 
-        static IList<Resource> _resources = new List<Resource>();
+		static readonly IList<Resource> _resources = new List<Resource>();
 
-        public static void AddResource(string baseName, Assembly assembly, bool first = false)
-        {
-            if (first)
-            {
-                _resources.Clear();
-                CultureInfo = null;
-            }
+		public static void AddResource(string baseName, Assembly assembly, bool first = false)
+		{
+			if (first)
+			{
+				_resources.Clear();
+				CultureInfo = null;
+			}
 
-            Resource resource = new Resource()
-            {
-                BaseName = baseName,
-                Assembly = assembly,
-            };
+			Resource resource = new Resource()
+			{
+				BaseName = baseName,
+				Assembly = assembly,
+			};
 
-            _resources.Add(resource);
-        }
+			_resources.Add(resource);
+		}
 
-        public static string GetText(string name)
-        {
-            if (name == null)
-                return "";
+		public static string GetText(string name)
+		{
+			if (name == null)
+				return "";
 
-            string value = null;
+			string value = null;
 
-            if (_resources.Count > 0)
-            {
-                foreach (var r in _resources)
-                {
-                    try
-                    {
-                        value = r.ResourceManager.GetString(name, CultureInfo);
-                        if (value != null)
-                            break;
-                    }
-                    catch { }
-                }
-            }
+			if (_resources.Count > 0)
+			{
+				foreach (var r in _resources)
+				{
+					try
+					{
+						value = r.ResourceManager.GetString(name, CultureInfo);
+						if (value != null)
+							break;
+					}
+					catch { }
+				}
+			}
 
-            if (value == null)
-                value = $"!@ {name} @!"; // returns the key, which GETS DISPLAYED TO THE USER
+			if (value == null)
+				value = $"!@ {name} @!"; // returns the key, which GETS DISPLAYED TO THE USER
 
-            return value;
-        }
-    }
+			return value;
+		}
+	}
 
-    /// <summary>
-    /// An auxiliary class used to the intuitive use of localized text in the code.
-    /// Example: string s = T.Localized("text id");
-    /// </summary>
-    public static class T
-    {
-        public static string Localized(string name)
-        {
-            return LocalizationManager.GetText(name);
-        }
-    }
+	/// <summary>
+	/// An auxiliary class used to the intuitive use of localized text in the code.
+	/// Example: string s = T.Localized("text id");
+	/// </summary>
+	public static class T
+	{
+		public static string Localized(string name)
+		{
+			return LocalizationManager.GetText(name);
+		}
+	}
 }
