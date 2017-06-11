31db2ad1e6654045fd7b2e55078decc669ef1881
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-11 16:27:40 -0400

    Next: Revision

--- pbXNet/pbXNet.AssemblyInfo.cs
+++ pbXNet/pbXNet.AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
