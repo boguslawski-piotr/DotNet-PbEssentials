848b77a69624d7ee4dde432e68a134cea1f72068
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-11 23:47:44 -0400

    Next: Revision

--- pbXForms/pbXForms.AssemblyInfo.cs
+++ pbXForms/pbXForms.AssemblyInfo.cs
@@ -13,5 +13,5 @@ using System.Reflection;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0.1")]
-[assembly: AssemblyFileVersion("1.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyFileVersion("1.0.0.2")]
