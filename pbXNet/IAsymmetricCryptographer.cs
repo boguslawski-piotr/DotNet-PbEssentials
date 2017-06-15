df276e2d3d57c7de23060651891e766c855136e5
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-15 12:51:53 -0400

    pbXNet: IAsymmetricCryptographer

---
+++ pbXNet/IAsymmetricCryptographer.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+
+namespace pbXNet
+{
+	public interface IAsymmetricCryptographer
+	{
+		(IByteBuffer pbl, IByteBuffer prv) GenerateKeyPair(int length = -1);
+
+		IByteBuffer Encrypt(IByteBuffer msg, IByteBuffer pblKey);
+
+		IByteBuffer Decrypt(IByteBuffer msg, IByteBuffer prvKey);
+
+		IByteBuffer Sign(IByteBuffer msg, IByteBuffer prvKey);
+
+		bool Verify(IByteBuffer msg, IByteBuffer signature, IByteBuffer pblKey);
+	}
+}
