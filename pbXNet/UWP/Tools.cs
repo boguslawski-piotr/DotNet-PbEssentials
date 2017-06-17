e7011c1c54996761331e36ecb12e36c88563a6bb
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-17 01:52:24 -0400

    pbXNet: Uaqpid for UWP (step 1)

--- pbXNet/UWP/Tools.cs
+++ pbXNet/UWP/Tools.cs
@@ -9,9 +9,23 @@ namespace pbXNet
 		static string _Uaqpid
 		{
 			get {
-				// TODO: _Uaqpid for UWP
-				string id = "b3fea4b6-0f44-466e-96e0-ba25324671fc";
-				string id2 = "UWP";
+				// MAX 10 passwords in valut per app
+				// can be used as SecureStorage? -> jeden passwd, w ktorym jest zapisane cale storage :)
+
+				var vault = new PasswordVault();
+
+				PasswordCredential cred = vault.Retrieve(resource, userName);
+				if (cred == null)
+				{
+					var cred = new PasswordCredential(resource, userName, new Password(Tools.CreateGuid()));
+					vault.Add(cred);
+				}
+
+				cred.RetrievePassword();
+				string id = cred.Password;
+				
+string id2 = "fd5d013709f94b4494fdeda98535fd32";
+
 				return id + id2;
 			}
 		}
