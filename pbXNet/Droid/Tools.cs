a6723d638e6a7bec0136b1bce66f6d76711e15e7
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-16 21:04:03 -0400

    pbXNet: fixed _Uaqpid for Android < M

--- pbXNet/Droid/Tools.cs
+++ pbXNet/Droid/Tools.cs
@@ -1,5 +1,6 @@
 ﻿#if __ANDROID__
 
+using Android.OS;
 using Android.Security.Keystore;
 using Java.Security;
 using Javax.Crypto;
@@ -11,27 +12,37 @@ namespace pbXNet
 		static string _Uaqpid
 		{
 			get {
-				const string KEY_NAME = ".a10a05d21b614485839508eeff4d00da";
-				const string KEYSTORE_NAME = "AndroidKeyStore";
-
-				KeyStore keystore = KeyStore.GetInstance(KEYSTORE_NAME);
-				keystore.Load(null);
-
-				if (!keystore.IsKeyEntry(KEY_NAME))
+				string id, id2;
+				if (Build.VERSION.SdkInt >= BuildVersionCodes.M)
 				{
-					KeyGenerator keyGenerator = KeyGenerator.GetInstance(KeyProperties.KeyAlgorithmHmacSha256, KEYSTORE_NAME);
-					keyGenerator.Init(new KeyGenParameterSpec.Builder(KEY_NAME, KeyStorePurpose.Sign).Build());
-					keyGenerator.GenerateKey();
+					const string KEY_NAME = ".a10a05d21b614485839508eeff4d00da";
+					const string KEYSTORE_NAME = "AndroidKeyStore";
+
+					KeyStore keystore = KeyStore.GetInstance(KEYSTORE_NAME);
+					keystore.Load(null);
+
+					if (!keystore.IsKeyEntry(KEY_NAME))
+					{
+						KeyGenerator keyGenerator = KeyGenerator.GetInstance(KeyProperties.KeyAlgorithmHmacSha256, KEYSTORE_NAME);
+						keyGenerator.Init(new KeyGenParameterSpec.Builder(KEY_NAME, KeyStorePurpose.Sign).Build());
+						keyGenerator.GenerateKey();
+					}
+
+					IKey secretKey = keystore.GetKey(KEY_NAME, null);
+					Mac mac = Mac.GetInstance(KeyProperties.KeyAlgorithmHmacSha256);
+					mac.Init(secretKey);
+					byte[] mid = mac.DoFinal();
+
+					id = mid.ToHexString();
+					id2 = Android.Provider.Settings.Secure.AndroidId;
+				}
+				else
+				{
+					id = Android.Provider.Settings.Secure.AndroidId;
+					id2 = "3AC2B00B1DB64686B388EEEF2F708009";
 				}
 
-				IKey secretKey = keystore.GetKey(KEY_NAME, null);
-				Mac mac = Mac.GetInstance(KeyProperties.KeyAlgorithmHmacSha256);
-				mac.Init(secretKey);
-				byte[] mid = mac.DoFinal();
-
-				string id = mid.ToHexString();
-				string id2 = Android.Provider.Settings.Secure.AndroidId;
-
+				Log.D($"id={id}, id2={id2}");
 				return id + id2;
 			}
 		}
