3ffd53c5c1776167031ce7f5ca8706cfe96da5fb
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-19 10:19:45 -0400

    Next: Revision

--- pbXForms/pbXForms.AssemblyInfo.cs
+++ pbXForms/pbXForms.AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Resources;
 // The form "{Major}.{Minor}.*" will automatically update the build and revision,
 // and "{Major}.{Minor}.{Build}.*" will update just the revision.
 
-[assembly: AssemblyVersion("1.0.0.18")]
-[assembly: AssemblyFileVersion("1.0.0.18")]
+[assembly: AssemblyVersion("1.0.0.19")]
+[assembly: AssemblyFileVersion("1.0.0.19")]
