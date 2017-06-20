e01a0ba533eee06874fdcd825d69471beba9c581
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-20 15:09:10 -0400

    Move NewtonsoftJsonSerializer from Notebooks (step 1)

---
+++ pbXNet/NewtonsoftJsonSerializer.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using pbXNet;
+
+namespace SafeNotebooks
+{
+	public class NewtonsoftJsonSerializer : ISerializer
+	{
+		JsonSerializerSettings _settings;
+
+		/// Settings to use with JsonConvert.Serialize i Deserialize methods.
+		public virtual JsonSerializerSettings Settings
+		{
+			get {
+				if (_settings == null)
+				{
+					_settings = new JsonSerializerSettings()
+					{
+						DateTimeZoneHandling = DateTimeZoneHandling.Utc,
+						NullValueHandling = NullValueHandling.Ignore,
+						//Formatting = Formatting.None,
+#if DEBUG
+						Formatting = Formatting.Indented,
+#endif
+					};
+				}
+				return _settings;
+			}
+
+			set {
+				_settings = value;
+			}
+		}
+
+		public string Serialize<T>(T o, string id = null)
+		{
+			string d = JsonConvert.SerializeObject(o, Settings);
+			if (id != null)
+			{
+				d = "'" + id + "':" + d + ",";
+			}
+			return d;
+		}
+
+		public T Deserialize<T>(string d, string id = null)
+		{
+			if (id != null)
+			{
+				JObject pd = JObject.Parse("{" + d + "}");
+				d = pd[id].ToString();
+			}
+
+			var o = JsonConvert.DeserializeObject<T>(d, Settings);
+			return o;
+		}
+
+	}
+}
