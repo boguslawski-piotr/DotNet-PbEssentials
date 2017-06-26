2f43f452eeff7cc0b3672744eb24ed0bf271c565
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-06-26 01:04:05 -0400

    pbXNet: StorageOnPbXStorage: let's begin...

---
+++ pbXNet/StorageOnPbXStorage.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace pbXNet
+{
+	[System.Serializable]
+	public class PbXStorageSettings
+	{
+		// For example: http[s]://[pbXStorage server address]:[pbXStorage server port]/api/storage/
+		public Uri ApiUri;
+
+		public IAsymmetricCryptographerKeyPair AppKeys;
+
+		public string ClientId;
+		public IAsymmetricCryptographerKeyPair ClientPublicKey;
+
+		public TimeSpan Timeout = TimeSpan.FromSeconds(30);
+	}
+
+	public class StorageOnPbXStorageException : Exception
+	{
+		public StorageOnPbXStorageException(string message)
+			: base(message)
+		{ }
+	}
+
+	public class StorageOnPbXStorage
+	{
+	}
+}
