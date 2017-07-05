b4e2cbdea743554f7f78b5428c98bfe9c4feb3d2
Piotr Boguslawski <boguslawski.piotr@gmail.com>
2017-07-04 21:40:00 -0400

    Transition to Xunit testing framework: iOS, UWP

--- Tests/iOS/Main.cs
+++ Tests/iOS/Main.cs
@@ -1,20 +1,15 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
+﻿using UIKit;
 
-using Foundation;
-using UIKit;
-
-using pbXNet;
-
-namespace Tests.iOS
+namespace iOS
 {
 	public class Application
 	{
 		// This is the main entry point of the application.
 		static void Main(string[] args)
 		{
-			UIApplication.Main(args, null, "UnitTestAppDelegate");
+			// if you want to use a different Application Delegate class from "AppDelegate"
+			// you can specify it here.
+			UIApplication.Main(args, null, "AppDelegate");
 		}
 	}
 }
